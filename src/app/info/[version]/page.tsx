137a6adea0b8a2f81d662d031cfef415fa869d07
Atanda uthman <atandauthman2@gmail.com>
2025-04-10 19:09:11 +0100

    ff

--- src/app/info/[version]/page.tsx
+++ src/app/info/[version]/page.tsx
@@ -13,7 +13,6 @@ import {
   Bar,
   BarChart,
   Cell,
-  Legend,
   Line,
   LineChart,
   ResponsiveContainer,
@@ -69,22 +68,6 @@ const Overview = () => {
       amt: 2100,
     },
   ];
-  const getPath = (x: any, y: any, width: any, height: any) => {
-    return `M${x},${y + height}C${x + width / 3},${y + height} ${
-      x + width / 2
-    },${y + height / 3}
-    ${x + width / 2}, ${y}
-    C${x + width / 2},${y + height / 3} ${x + (2 * width) / 3},${y + height} ${
-      x + width
-    }, ${y + height}
-    Z`;
-  };
-
-  const TriangleBar = (props: any) => {
-    const { fill, x, y, width, height } = props;
-
-    return <path d={getPath(x, y, width, height)} stroke="none" fill={fill} />;
-  };
 
   return (
     <div className=" text-white  ">
@@ -140,8 +123,8 @@ const Overview = () => {
               />
               <Bar
                 dataKey="uv"
-                fill="#8884d8"
-                shape={<TriangleBar />}
+                fill="#00b33c"
+                // shape={<TriangleBar />}
                 // label={{ position: "top" }}
               >
                 {data.map((entry, index) => (
